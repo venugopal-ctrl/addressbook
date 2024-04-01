420ef36b4132f0d1d6a63ab44df87942e98bb21b
venugopal-ctrl <53956399+venugopal-ctrl@users.noreply.github.com>
2024-04-01 09:14:07 +0530

    updated the config file

--- server-config.sh
+++ server-config.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
 sudo yum install git -y
-sudo yum install maven -y
-sudo yum install java-1.8.0-openjdk-devel -y 
+sudo yum install docker -y
+sudo systemctl start docker
+#sudo yum install maven -y
+#sudo yum install java-1.8.0-openjdk-devel -y 
 
 if [ -d "addressbook" ]
 then
@@ -15,7 +17,8 @@ else
    git checkout features/devops1
 fi
 
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.402.b06-1.amzn2.0.1.x86_64
-export PATH=$JAVA_HOME/bin:$PATH
-source /etc/profile
-mvn package
+#export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.402.b06-1.amzn2.0.1.x86_64
+#export PATH=$JAVA_HOME/bin:$PATH
+#source /etc/profile
+#mvn package
+sudo docker build -t $1:$2 /home/ec2-user/addressbook
